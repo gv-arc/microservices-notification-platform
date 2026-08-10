d2c6c5403a340c0c605380f07dc2017032b0fa93
Gaurav singh <gauravss720@gmail.com>
2026-08-10 18:36:48 +0530

    Update init.sql

--- infrastructure/mysql/init.sql
+++ infrastructure/mysql/init.sql
@@ -1,11 +1,4 @@
--- Initialize databases and users for local development
+-- Initialize databases for local development
+
 CREATE DATABASE IF NOT EXISTS user_db;
 CREATE DATABASE IF NOT EXISTS notification_db;
-
-CREATE USER IF NOT EXISTS 'user_app'@'%' IDENTIFIED BY 'user_secret';
-CREATE USER IF NOT EXISTS 'notification_app'@'%' IDENTIFIED BY 'notification_secret';
-
-GRANT ALL PRIVILEGES ON user_db.* TO 'user_app'@'%';
-GRANT ALL PRIVILEGES ON notification_db.* TO 'notification_app'@'%';
-
-FLUSH PRIVILEGES;
